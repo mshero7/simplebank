54ad0a0df2eaecde70679cacccd5afad3edb0f20
mshero7 <mshero7@naver.com>
2023-01-27 14:38:05 +0900

    1. account table CRUD test 2. entry, transfer sql generate / sqlc generate

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+)
+
+// Queries 에 method 들로 붙어있다
+var testQueries *Queries
+
+// test main entry point
+// test할때 TestMain은 무조건 탄다
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
---
+++ util/random.go
@@ -0,0 +1,49 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+// set the seed value
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// RandomInt
+// min <= val <= max
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+// RandomString
+// 길이 n인 임의 문자열 반환
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn((k))]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"EUR", "USD", "CAD"}
+	n := len(currencies)
+
+	return currencies[rand.Intn(n)]
+}
