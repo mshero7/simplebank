622c723a07756a385fab0597acc2090e7b2c257d
Sangsu Moon <mshero7@naver.com>
2023-02-15 16:19:40 +0900

    jwt, paseto token auth

---
+++ token/jwt_maker.go
@@ -0,0 +1,63 @@
+package token
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+const minSecretKeySize = 32
+
+type JWTMaker struct {
+	secretKey string
+}
+
+func NewJwtMaker(secretkey string) (Maker, error) {
+	if len(secretkey) < minSecretKeySize {
+		return nil, fmt.Errorf("invalid key size: must be at least %d character", minSecretKeySize)
+	}
+
+	return &JWTMaker{secretKey: secretkey}, nil
+}
+
+func (m *JWTMaker) CreateToken(username string, duration time.Duration) (string, error) {
+	payload, err := NewPayload(username, duration)
+	if err != nil {
+		return "", err
+	}
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
+
+	return jwtToken.SignedString([]byte(m.secretKey))
+}
+
+func (m *JWTMaker) VerifyToken(token string) (*Payload, error) {
+	// 파싱된 검증되지않은 토큰을 검증해주는 ParseWithClaims용 콜백함수
+	keyFunc := func(token *jwt.Token) (interface{}, error) {
+		// 받은 토큰의 알고리즘이 맞는지 확인
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, ErrInvalidToken
+		}
+		return []byte(m.secretKey), nil
+	}
+	jwtToken, err := jwt.ParseWithClaims(token, &Payload{}, keyFunc)
+	// 1. 만료되었거나 2. 유효하지 않은 정보거나
+	if err != nil {
+		verr, ok := err.(*jwt.ValidationError)
+		if ok && errors.Is(verr.Inner, ErrExpiredToken) {
+			return nil, ErrExpiredToken
+		}
+
+		return nil, ErrInvalidToken
+	}
+
+	payload, ok := jwtToken.Claims.(*Payload)
+	if !ok {
+		return nil, ErrInvalidToken
+	}
+
+	return payload, nil
+}
