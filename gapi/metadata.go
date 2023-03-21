8e12467ea1e48beceeeacf2e6f30671c7f8f58b1
Sangsu Moon <mshero7@naver.com>
2023-03-21 13:16:46 +0900

    grpc metadata

---
+++ gapi/metadata.go
@@ -0,0 +1,47 @@
+package gapi
+
+import (
+	"context"
+	"log"
+
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/peer"
+)
+
+const (
+	grpcGatewayUserAgentHeader = "grpcgateway-user-agent"
+	userAgentHeader            = "user-agent"
+	xForwardedForHeader        = "x-forwared-for"
+)
+
+// keep track of all metadata we want to extract from the context
+type Metadata struct {
+	UserAgent string
+	ClientIP  string
+}
+
+func (server *Server) extractMetadata(ctx context.Context) *Metadata {
+	mtdt := &Metadata{}
+
+	if md, ok := metadata.FromIncomingContext(ctx); ok {
+		log.Printf("%+v", md)
+		if userAgents := md.Get(grpcGatewayUserAgentHeader); len(userAgents) > 0 {
+			mtdt.UserAgent = userAgents[0]
+		}
+
+		if userAgents := md.Get(userAgentHeader); len(userAgents) > 0 {
+			mtdt.UserAgent = userAgents[0]
+		}
+
+		if clientIPs := md.Get(xForwardedForHeader); len(clientIPs) > 0 {
+			mtdt.ClientIP = clientIPs[0]
+		}
+	}
+
+	// grpc ip 정보는 위에서 가져올수 없어서 아래를 활용해야함.
+	if p, ok := peer.FromContext(ctx); ok {
+		mtdt.ClientIP = p.Addr.String()
+	}
+
+	return mtdt
+}
--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -40,12 +40,14 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 		return nil, status.Errorf(codes.Internal, "Failed to create refresh token")
 	}
 
+	mtdt := server.extractMetadata(ctx)
+
 	session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
 		ID:           refreshPayload.ID,
 		Username:     user.Username,
 		RefreshToken: refreshToken,
-		UserAgent:    "",
-		ClientIp:     "",
+		UserAgent:    mtdt.UserAgent,
+		ClientIp:     mtdt.ClientIP,
 		IsBlocked:    false,
 		ExpiredAt:    refreshPayload.ExpiredAt,
 	})
